b00d8989c00111e5f7dd4dac15c12d4d231a70d5
Jake Franklin <jacob.d.franklin@gmail.com>
2018-01-13 12:55:53 -0700

    converted everything to an ES6 class, in order to work better with dat.GUI

--- runes.js
+++ runes.js
@@ -2,154 +2,148 @@
 // SENTENCES are made up of WORDS are made up of LETTERS
 // LETTERS are grids of dots, which are connected to each other by random lines
 
-// Editable Variables
-let sentences = 6;      // Number of sentences to write (only affects up until the end of the canvas; vertical overflow is cut off)
-const words = [3, 10];    // {min, max] no. of words per line
-const letters = [2, 10];  // {min, max] no. of letters per word
+class Runes {
+    constructor(options) {
+        // Options
+        const defaults = {
+            el: document.getElementById("sheet"),
+            height: 500,
+            width: 1000,
+            padding: 100,
+            sentences: 6,
+            words: [3, 10],
+            letters: [2, 10],
+            points: [[0, 0], [10, 0], [20, 0], [0, 10], [10, 10], [20, 30]], // This defines the 'grid' for each letter, where [0, 0] is the top left corner
+            variance: 1,
+            letterWidth: 20,
+            letterHeight: 20,
+            wordSpacing: 40,
+            lineFeed: 50,
+        };
 
-const points = [[0, 0], [10, 0], [20, 0], [0, 10], [10, 10], [20, 30]]; // This defines the 'grid' for each letter, where [0, 0] is the top left corner
-const variance = 1; // How far line start/end points should deviate from the grid
+        Object.assign(this, defaults, options);
 
-const letterWidth = 20; // Left of one letter → left of the next
-const letterHeight = 20; // Affects last line overflow; if unclear just set it to the highest y-value in points[][];
-const wordSpacing = 40; // Space to insert between words
-const lineFeed = 50; // Bottom of one line to bottom of the next. Adobe apps call this 'leading' but they shouldn't.
-
-const height = 500;
-const width = 1000;
-const padding = 100; // Page margins
-
-// System Variables
-let finished = false;
-let finishedParagraph = false;
-let m_words = 0;
-let m_letters = 0;
-let cursor_x = padding;
-let cursor_y = padding;
-
-// Utilities
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; // min and max are both inclusive
-}
-
-// Setup
-function setup() {
-    // const background = 29;    // RGB value of the background
-    // const strokeStyle = 220;       // RGB value of the writing
-    const dark = `${random(100, 255)}, ${random(100, 255)}, ${random(100, 255)}`;
-    const light = `${random(0, 40)}, ${random(0, 40)}, ${random(0, 40)}`;
-    const colorScheme = Math.random() < 0.5;
-    const lineWidth = 1;   // Weight (in pixels) of the writing
-    let canvas = document.getElementById("sheet");
-    let ctx = canvas.getContext('2d');
-
-    canvas.width = width;
-    canvas.height = height;
-    canvas.style.backgroundColor = colorScheme ? `rgb(${light})` : `rgb(${dark})`;
-    document.body.style.backgroundColor = colorScheme ? `rgb(${light})` : `rgb(${dark})`;
-
-    ctx.strokeStyle = colorScheme ? `rgb(${dark})` : `rgb(${light})`;
-    ctx.lineWidth = lineWidth;
-}
-
-function draw() {
-  while(!finishedParagraph) {
-    AddSentence();
-  }
-}
-
-function AddSentence() {
-  if(sentences <= 0) {
-    finishedParagraph = true;
-  }
-
-  if(!finished) {
-    AddWord();
-  } else {
-    sentences -= 1;
-    finished = false;
-    m_words = random(words[0], words[1]);
-    cursor_x += wordSpacing;
-    AddWord();
-  }
-}
-
-function AddWord() {
-  m_letters = random(letters[0], letters[1]);
-
-  if(m_words > 0) {
-    m_words--;
-  } else if(m_words == 0) {
-    finished = true;
-  }
-
-  for(let i = 0, l = m_letters; i < l; i ++) {
-
-    if(cursor_x + (letterWidth * m_letters) >= width - padding) {
-      cursor_x = padding;
-      cursor_y += lineFeed; // Line feed
+        this.finished = false;
+        this.finishedParagraph = false;
+        this.m_words = 0;
+        this.m_letters = 0;
+        this.cursor_x = this.padding;
+        this.cursor_y = this.padding;
     }
 
-    if(cursor_y <= height - padding - letterHeight) {
-      AddLetter();
-    } else {
-      finishedParagraph = true;
-      return;
+    static random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min; // min and max are both inclusive
     }
-  }
 
-  cursor_x += letterWidth;
-}
+    init() {
+        const dark = `${Runes.random(100, 255)}, ${Runes.random(100, 255)}, ${Runes.random(100, 255)}`;
+        const light = `${Runes.random(0, 40)}, ${Runes.random(0, 40)}, ${Runes.random(0, 40)}`;
+        const colorScheme = Math.random() < 0.5;
+        const lineWidth = 1;   // Weight (in pixels) of the writing
 
-function AddLetter() {
-  const canvas = document.getElementById("sheet");
-  let ctx = canvas.getContext('2d');
+        let ctx = this.el.getContext('2d');
 
-  let p = points.map(point => point.slice()); // so that we can manipulate the point grid for just this letter
+        this.el.width = this.width;
+        this.el.height = this.height;
+        this.el.style.backgroundColor = colorScheme ? `rgb(${light})` : `rgb(${dark})`;
+        document.body.style.backgroundColor = colorScheme ? `rgb(${light})` : `rgb(${dark})`;
 
-  if(Math.random() < 0.1) {
-    p[3][1] *= 3;
-  }
+        ctx.strokeStyle = colorScheme ? `rgb(${dark})` : `rgb(${light})`;
+        ctx.lineWidth = this.lineWidth;
+    }
 
-  for(let i = 0, l = random(2, 4); i < l; i ++) {
-    let firstPoint = random(0, p.length - 1);
-    let secondPoint = random(0, p.length - 1);
-    let thirdPoint = random(0, p.length - 1);
+    draw() {
+        while(!this.finishedParagraph) this.addSentence();
+    }
 
-    while(firstPoint == secondPoint || secondPoint == thirdPoint || secondPoint >= p.length || secondPoint < 0) { // Reroll if duplicate
-      if(secondPoint >= p.length) {
-        secondPoint--;
-    } else if(secondPoint < 0) {
-        secondPoint++;
-    } else {
-        Math.random() > 0.5 ? secondPoint++ : secondPoint--;
+    addSentence() {
+        if(this.sentences <= 0)
+            this.finishedParagraph = true;
+
+        if(!this.finishedSentence) {
+            this.addWord();
+        } else {
+            this.sentences -= 1;
+            this.finishedSentence = false;
+            this.m_words = Runes.random(this.words[0], this.words[1]);
+            this.cursor_x += this.wordSpacing;
+            this.addWord();
+        }
+    }
+
+    addWord() {
+        this.m_letters = Runes.random(this.letters[0], this.letters[1]);
+
+        if(this.m_words > 0)
+            this.m_words--;
+        else if(this.m_words == 0)
+            this.finishedSentence = true;
+
+      for(let i = 0, l = this.m_letters; i < l; i ++) {
+
+        if(this.cursor_x + (this.letterWidth * this.m_letters) >= this.width - this.padding) {
+          this.cursor_x = this.padding;
+          this.cursor_y += this.lineFeed; // Line feed
+        }
+
+        if(this.cursor_y <= this.height - this.padding - this.letterHeight) {
+          this.addLetter();
+        } else {
+          this.finishedParagraph = true;
+          return;
+        }
       }
+
+      this.cursor_x += this.letterWidth;
     }
 
-    if(!p[secondPoint])
-        debugger;
+    addLetter() {
+        let ctx = this.el.getContext('2d');
 
-    const x1 = p[firstPoint][0] + cursor_x + random(-variance, variance);
-    const y1 = p[firstPoint][1] + cursor_y + random(-variance, variance);
-    const x2 = p[secondPoint][0] + cursor_x + random(-variance, variance);
-    const y2 = p[secondPoint][1] + cursor_y + random(-variance, variance);
-    const x3 = p[thirdPoint][0] + cursor_x + random(-variance, variance);
-    const y3 = p[thirdPoint][1] + cursor_y + random(-variance, variance);
+        let p = this.points.map(point => point.slice()); // so that we can manipulate the point grid for just this letter
 
-    if(Math.random() < 0.4) { // Draw a linear connection 40% of the time
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x2, y2);
-      ctx.stroke();
-      ctx.closePath();
-    } else { // Draw a curved connection 60% of the time
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.bezierCurveTo(x1, y1, x3, y3, x2, y2);
-      ctx.stroke();
-      ctx.closePath();
+        if(Math.random() < 0.1)
+            p[3][1] *= 3;
+
+        for(let i = 0, l = Runes.random(2, 4); i < l; i ++) {
+            let firstPoint = Runes.random(0, p.length - 1);
+            let secondPoint = Runes.random(0, p.length - 1);
+            let thirdPoint = Runes.random(0, p.length - 1);
+
+            while(firstPoint == secondPoint || secondPoint == thirdPoint || secondPoint >= p.length || secondPoint < 0) { // Reroll if duplicate
+                if(secondPoint >= p.length)
+                    secondPoint--;
+                else if(secondPoint < 0)
+                    secondPoint++;
+                else
+                    Math.random() > 0.5 ? secondPoint++ : secondPoint--;
+            }
+
+            if(!p[secondPoint])
+                debugger;
+
+            const x1 = p[firstPoint][0] + this.cursor_x + Runes.random(-this.variance, this.variance);
+            const y1 = p[firstPoint][1] + this.cursor_y + Runes.random(-this.variance, this.variance);
+            const x2 = p[secondPoint][0] + this.cursor_x + Runes.random(-this.variance, this.variance);
+            const y2 = p[secondPoint][1] + this.cursor_y + Runes.random(-this.variance, this.variance);
+            const x3 = p[thirdPoint][0] + this.cursor_x + Runes.random(-this.variance, this.variance);
+            const y3 = p[thirdPoint][1] + this.cursor_y + Runes.random(-this.variance, this.variance);
+
+            if(Math.random() < 0.4) { // Draw a linear connection 40% of the time
+                ctx.beginPath();
+                ctx.moveTo(x1, y1);
+                ctx.lineTo(x2, y2);
+                ctx.stroke();
+                ctx.closePath();
+            } else { // Draw a curved connection 60% of the time
+                ctx.beginPath();
+                ctx.moveTo(x1, y1);
+                ctx.bezierCurveTo(x1, y1, x3, y3, x2, y2);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+
+        this.cursor_x += this.letterWidth;
     }
-  }
-
-  cursor_x += letterWidth;
 }
